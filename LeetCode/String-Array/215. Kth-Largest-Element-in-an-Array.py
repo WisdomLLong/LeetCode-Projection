0395e8f91952461bd9f9d6f7ec683da484b82948
WisdomLLong <f_l_wang@163.com>
2017-12-14 21:15:36 +0800

    Update 215. Kth-Largest-Element-in-an-Array.py

--- LeetCode/String-Array/215. Kth-Largest-Element-in-an-Array.py
+++ LeetCode/String-Array/215. Kth-Largest-Element-in-an-Array.py
@@ -8,3 +8,32 @@ Note:
 You may assume k is always valid, 1 ≤ k ≤ array's length.
 '''
 
+'''
+堆的用法
+'''
+
+#########################################################
+# Better Solution
+#########################################################
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+
+        heap = []
+
+        for num in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
+            else:
+                if heap[0] <= num:
+                    heapq.heappushpop(heap, num)
+
+        return heap[0]
