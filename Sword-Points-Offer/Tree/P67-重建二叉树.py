7e4d9322540d25c57e2d1378a91024a22aad87d7
WisdomLLong <f_l_wang@163.com>
2017-12-15 21:50:57 +0800

    Create P67-重建二叉树.py

---
+++ Sword-Points-Offer/Tree/P67-重建二叉树.py
@@ -0,0 +1,25 @@
+''''' 
+重建二叉树 
+题目描述 
+输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。 
+例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。 
+'''  
+
+class Tree(object):
+    def __init__(self, data):
+        self.tree = data
+        self.tleft = None
+        self.tright = None
+        
+class TwoTree(object):
+    def reConstructBinaryTree(self, pre, tin):
+        if len(pre)==0:
+            return None
+        if len(pre)==1:
+            return Tree(pre[0])
+        else:
+            res = Tree(pre[0])
+            res.tleft = self.reConstructBinaryTree(pre[1:tin.index(pre[0])+1], tin[:tin.index(pre[0])])
+            res.tright = self.reConstructBinaryTree(pre[tin.index(pre[0])+1:], tin[tin.index(pre[0])+1:])
+        return res   
+TwoTree().reConstructBinaryTree([1,2,4,7,3,5,6,8], [4,7,2,1,5,3,8,6])
